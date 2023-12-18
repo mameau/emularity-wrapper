7a1fa5b797c4d0359fa834974721fc81f09da9f9
sairuk <sairuk@gmail.com>
2023-12-18 22:40:31 +1100

    playing around

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # emularity-wrapper
 messing around with emularity
+
+wrapper for emularity, see index.php (yes, yes) for examples of loading titles. nothing here is secured so use at your own risk.
+
+## setup
+1. clone this repo
+2. clone emularity report as sub directory
+3. host the site (php required atm)
+4. add roms and software
+4. play some games
+
+## layout
+```
+/
+├── assets
+│   ├── autoboot
+│   └── hash
+├── binaries
+│   ├── machines
+│   ├── roms
+│   └── software
+│       ├── apple2_flop_orig
+│       ├── psx
+│       │   └── thps2
+│       ├── sms
+
+```
+
+## folders
+```
+autoboot    -> lua scripts
+hass        -> mame softlists
+machine     -> wasm/js files
+rom         -> machine files
+software    -> software files
+```
+
+## get params
+```
+m       -> machine
+a[]     -> assets
+r[]     -> roms
+s[]     -> software
+sl[]    -> software lists
+rw      -> width
+rh      -> height
+sc      -> scale
+p[s]    -> peripheral key->slot, value->attachment
+cli[]   -> custom cli options (extraArgs)
+```
+
+
+## basic requirements for a machine with software
+1. the wasm/js machine files from MAME
+2. load the driver
+3. mount machine roms
+4. mount additional software and/or assets 
+5. attach software to slots
+6. start the machine
---
+++ jsmame-load_func.php
@@ -0,0 +1,60 @@
+<?php
+$machine = filter_input(INPUT_GET, 'm', FILTER_SANITIZE_SPECIAL_CHARS);
+
+# unsafe
+isset($_GET['r']) ? $roms = $_GET['r'] : $roms = array() ;
+isset($_GET['a']) ? $assets = $_GET['a'] : $assets = array();
+isset($_GET['s']) ? $software = $_GET['s'] : $software = array();
+isset($_GET['sl']) ? $softwarelist = $_GET['sl'] : $softwarelist = array();
+isset($_GET['sc']) ? $scale = $_GET['sc'] : $scale = 3;
+isset($_GET['rw']) ? $resw = $_GET['rw'] : $resw = 256;
+isset($_GET['rh']) ? $resh = $_GET['rh'] : $resh = 224;
+isset($_GET['p']) ? $peripherals = $_GET['p'] : $peripherals = array();
+isset($_GET['cli']) ? $cli = $_GET['cli'] : $cli = array();
+
+$cliDefaults = "-verbose -ui_active -lowlatency -nowaitvsync -nofilter -renderdriver opengles2";
+$cliMerge = implode(" ", $cli);
+$cliArgs = "$cliDefaults $cliMerge";
+
+## mount
+#
+# mount an item into the emulator
+#
+function mount($name="nonlisted.zip",$path="assets") {
+	echo('
+		JSMAMELoader.mountFile("'.basename($name).'",
+					JSMAMELoader.fetchFile("'.basename($name).'",
+								"'.$path.'/'.$name.'")),
+	    ');
+}
+
+## mount_all
+#
+# loop over an array of items to mount into the emulator
+#
+function mount_all($array, $path){
+	if ( $array != NULL )
+	{
+		foreach ( $array as $item ) {
+			mount($item, $path);
+		}
+	}
+}
+
+## peripheral
+#
+# attach items to slots based on array
+# key = slot (i.e. cart)
+# value = item
+#
+function peripheral($array) {
+	if ( $array != NULL )
+	{
+		foreach ( $array as $key => $item ) {
+			echo('JSMAMELoader.peripheral("'.$key.'","'.$item.'"),');
+		}
+	}
+}
+
+?>
+
