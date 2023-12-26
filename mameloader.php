e0ca9622ed19a7e7682e357a859cd04c06857be3
sairuk <sairuk@gmail.com>
2023-12-26 15:56:49 +1100

    use fork, tweak stuff

--- mameloader.php
+++ mameloader.php
@@ -4,7 +4,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title><?php echo $machine ?></title>
+		<title><?php echo basename($machine); ?></title>
 	</head>
 	<body style="background: black; font-size:16px;">
 		<canvas id="canvas" style="background-color: black; width: 50%; height: 50%; display: block; margin: 0 auto;"></canvas>
@@ -16,7 +16,7 @@
 			var cliArgs = "<?php echo $cliArgs ?>";
 			var emulator = new Emulator(document.querySelector("#canvas"),
 							null,
-							new MAMELoader(MAMELoader.driver(machine),
+							new MAMELoader(MAMELoader.driver("<?php echo basename($machine) ?>"),
 							MAMELoader.nativeResolution(<?php echo "$resw,$resh" ?>),
 							MAMELoader.scale(<?php echo $scale ?>),
 							MAMELoader.keepAspect(1),
@@ -27,7 +27,7 @@
 							<?php mount_all($roms,"binaries/roms"); ?>
 							<?php mount_all($software,"binaries/software"); ?>
 							<?php mount_all($softwarelist,"assets/hash"); ?>
-							<?php mount_all($state,"binaries/state/$machine"); ?>
+							<?php mount_all($state,"binaries/state/" . basename($machine)); ?>
 			));
 			emulator.start({ waitAfterDownloading: false });
 
